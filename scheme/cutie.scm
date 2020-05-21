2da2f794c15e2f6ea406c5a177a1fb2e1585c2a9
Faye <justalittleepsilon@gmail.com>
2020-05-21 03:35:13 -0500

    Fix some pronouns stuff (thanks @RX14)

--- scheme/cutie.scm
+++ scheme/cutie.scm
@@ -1,16 +1,16 @@
 ; Make sure to establish boundaries ahead of time! If you don't know, assume you can't, and just ask <3
-(define (make-cutie some-name pronoun1 pronoun2)
-  `((name . ,some-name) (pronouns . (,pronoun1 . ,pronoun2)) (listen? . ,#f) (hug? . ,#f) (give-advice? . ,#f)))
+(define (make-cutie some-name pronoun1 pronoun2 pronoun3)
+  `((name . ,some-name) (pronouns . (,pronoun1 . (,pronoun2 . ,pronoun3))) (listen? . ,#f) (hug? . ,#f) (give-advice? . ,#f)))
 
 (define (listening do? pronouns)
   (if do?
-      (string-append " appreciates you listening to " (cdr pronouns))
+      (string-append " appreciates you listening to " (cadr pronouns))
       (string-append " is thankful " (car pronouns) " doesn't have to explain the situation right now")))
 
 (define (hugging do? pronouns)
   (if do?
       ", especially with the accompanying hugs"
-      (string-append " and respecting " (cdr pronouns) " boundaries")))
+      (string-append " and respecting " (cddr pronouns) " boundaries")))
 
 (define (advice do? pronouns)
   (if do?
@@ -25,27 +25,27 @@
      (hugging (assq-ref cutie 'hug?) pronouns)
      (advice (assq-ref cutie 'give-advice?) pronouns))))
 
-(define cassie (make-cutie "Cassie" "she" "her"))
+(define cassie (make-cutie "Cassie" "she" "her" "her"))
 (assq-set! cassie 'listen? #t)
 (assq-set! cassie 'hug? #t)
 (assq-set! cassie 'give-advice? #f)
 
-(define stephanie (make-cutie "Stephanie" "she" "her"))
+(define stephanie (make-cutie "Stephanie" "she" "her" "her"))
 (assq-set! stephanie 'listen? #t)
 (assq-set! stephanie 'hug? #t)
 (assq-set! stephanie 'give-advice? #t)
 
-(define meri (make-cutie "Merbear" "she" "her"))
+(define meri (make-cutie "Merbear" "she" "her" "her"))
 (assq-set! meri 'listen? #t)
 (assq-set! meri 'hug? #t)
 (assq-set! meri 'give-advice? #t)
 
-(define lun (make-cutie "LUN-4" "it" "it"))
+(define lun (make-cutie "LUN-4" "it" "it" "its"))
 (assq-set! lun 'listen? #f)
 (assq-set! lun 'hug? #t)
 (assq-set! lun 'give-advice? #f)
 
-(define blair (make-cutie "Blair" "she" "her"))
+(define blair (make-cutie "Blair" "she" "her" "her"))
 (assq-set! blair 'listen? #t)
 (assq-set! blair 'hug? #t)
 (assq-set! blair 'give-advice? #f)
